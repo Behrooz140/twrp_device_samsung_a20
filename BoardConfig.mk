19406af1867cb17f7be60aab03f7109927058b80
Behrooz140 <122544608+Behrooz140@users.noreply.github.com>
2026-09-14 13:57:09 +0330

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,3 +97,6 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_BRIGHTNESS_PATH := /sys/class/backlight/panel/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 96
